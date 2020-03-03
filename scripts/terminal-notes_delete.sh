58ef9af5813de96d66581c9cd35665a1bd335b1f
zahdr <f.wischendorff@posteo.de>
2020-03-03 17:18:13 +0100

    added delete and show function

--- scripts/terminal-notes_delete.sh
+++ scripts/terminal-notes_delete.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+TNN_DIR=$HOME/.terminal-notes/notes
+NUMBER=1
+
+
+# delete specified note
+for f in $TNN_DIR/*
+do
+  if [ "$1" = "$NUMBER" ]
+  then
+    echo "Deleted note $NUMBER - $(cat $f)"
+    rm -rf $f
+  fi
+  ((NUMBER++))
+done
--- scripts/terminal-notes_show.sh
+++ scripts/terminal-notes_show.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+TNN_DIR=$HOME/.terminal-notes/notes
+NUMBER=1
+
+
+# show all notes and their filename
+for f in $TNN_DIR/*
+do
+  echo ""
+  echo "-----"
+  echo "| $NUMBER |"
+  echo "-----"
+  cat $f
+  echo ""
+  ((NUMBER++))
+done
+
+echo ""
+echo ""
