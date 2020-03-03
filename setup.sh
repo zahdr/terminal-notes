c88dbe418cd6578be77f25c318993c4746e229c3
zahdr <f.wischendorff@posteo.de>
2020-03-03 17:50:32 +0100

    uncommented alias creation

--- setup.sh
+++ setup.sh
@@ -13,17 +13,17 @@ fi
 cp -R scripts/ $TN_DIR/scripts/
 chmod +x $TN_DIR/scripts/terminal-notes_*
 
-# # set alias
-# echo "alias an='sh $TN_DIR/scripts/terminal-notes_add.sh'" >> $HOME/.bash_profile
-# echo "alias dn='sh $TN_DIR/scripts/terminal-notes_delete.sh'" >> $HOME/.bash_profile
-# echo "alias sn='sh $TN_DIR/scripts/terminal-notes_show.sh'" >> $HOME/.bash_profile
-#
-# if [ -e "$ZSH_PROFILE" ]
-# then
-#   echo "ist da"
-#   echo "alias an='sh $TN_DIR/scripts/terminal-notes_add.sh'" >> $ZSH_PROFILE
-#   echo "alias dn='sh $TN_DIR/scripts/terminal-notes_delete.sh'" >> $ZSH_PROFILE
-#   echo "alias sn='sh $TN_DIR/scripts/terminal-notes_show.sh'" >> $ZSH_PROFILE
-# fi
+# set alias
+echo "alias an='sh $TN_DIR/scripts/terminal-notes_add.sh'" >> $HOME/.bash_profile
+echo "alias dn='sh $TN_DIR/scripts/terminal-notes_delete.sh'" >> $HOME/.bash_profile
+echo "alias sn='sh $TN_DIR/scripts/terminal-notes_show.sh'" >> $HOME/.bash_profile
+
+if [ -e "$ZSH_PROFILE" ]
+then
+  echo "ist da"
+  echo "alias an='sh $TN_DIR/scripts/terminal-notes_add.sh'" >> $ZSH_PROFILE
+  echo "alias dn='sh $TN_DIR/scripts/terminal-notes_delete.sh'" >> $ZSH_PROFILE
+  echo "alias sn='sh $TN_DIR/scripts/terminal-notes_show.sh'" >> $ZSH_PROFILE
+fi
 
 echo "Setup complete..."
